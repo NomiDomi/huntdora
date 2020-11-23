66ec3600f09e1cb16cf89d7b8abcf4e4a414fc72
Noemi <gali.k.noemi@gmail.com>
2020-11-23 13:14:15 +0100

    Added text unit test on <EmptyList />

---
+++ client/src/components/EmptyList.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { EmptyList } from './EmptyList';
+import { Typography } from '@material-ui/core';
+import { createShallow } from '@material-ui/core/test-utils';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<EmptyList />', () => {
+  let shallow: any;
+
+  beforeAll(() => {
+    shallow = createShallow();
+  });
+
+  it('should show text', () => {
+    const wrapper = shallow(<EmptyList />);
+    const text = wrapper.find(Typography);
+    expect(text.text()).toBe("No Jobs Here!");
+  })
+});
