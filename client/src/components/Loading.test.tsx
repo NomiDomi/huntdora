6bf71109a81c4b96aa4dbe6dd29cbfa864d40c64
Noemi <gali.k.noemi@gmail.com>
2020-11-23 13:12:11 +0100

    Added text unit test on <Loading />

---
+++ client/src/components/Loading.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Loading } from './Loading';
+import { Typography } from '@material-ui/core';
+import { createShallow } from '@material-ui/core/test-utils';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Loading />', () => {
+  let shallow: any;
+
+  beforeAll(() => {
+    shallow = createShallow();
+  });
+
+  it('should show text', () => {
+    const wrapper = shallow(<Loading />);
+    const text = wrapper.find(Typography);
+    expect(text.text()).toBe("Getting Jobs...");
+  })
+});
